aa74819d8b0d6b8261473e55495eedec755966df
huaphuthien7a1 <huaphuthienksst@gmail.com>
2021-06-08 21:05:08 +0700

    update modal

--- src/component/HomeTemplate/DetailPageComponent/DetailTop/DetailTop.js
+++ src/component/HomeTemplate/DetailPageComponent/DetailTop/DetailTop.js
@@ -2,61 +2,16 @@ import React, { memo, useState } from "react";
 import styled from "styled-components";
 import DetailTopLeft from "./DetailTopLeft";
 import DetailTopRight from "./DetailTopRight";
-import { actHandleModal } from "redux/actions/movieActions";
-import { useDispatch } from "react-redux";
-function DetailTop(props) {
-  const DetailTop = styled.div`
-     {
-      box-sizing: border-box;
-      overflow: hidden;
-      margin-top: 4rem;
-      z-index: 1;
-      max-height: 30rem;
 
-      &::after {
-        content: "";
-        display: block;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        background: linear-gradient(to top, rgb(10, 32, 41), transparent 100%);
-      }
-    }
-    @media (max-width: 736px) {
-      display: none;
-    }
-  `;
-  const DetailTop__mobie = styled.div`
-    overflow: hidden;
-    display: none;
+import ModalComponent from "./../../Modal/Modal";
 
-    @media (max-width: 736px) {
-      display: block;
-    }
-  `;
-  const ImgBackground = styled.img`
-    filter: blur(15px);
-    margin: -50% 0 -5px 0px;
-    display: block;
-    width: 100%;
-  `;
-  const ImgBackgroundMobie = styled.img`
-    margin: -50% 0 -5px 0px;
-    display: block;
-    width: 100%;
-  `;
-  const Container__ImgBackGroundMobie = styled.div.attrs((props) => ({
-    Height: props.height,
-  }))`
-    dislay: none !important;
+const DetailTop_Desktop = styled.div`
+   {
+    box-sizing: border-box;
     overflow: hidden;
     margin-top: 4rem;
-    position: relative;
     z-index: 1;
-    overflow: hidden;
-    height: ${(props) => props.Height};
+    max-height: 30rem;
 
     &::after {
       content: "";
@@ -68,70 +23,117 @@ function DetailTop(props) {
       left: 0;
       background: linear-gradient(to top, rgb(10, 32, 41), transparent 100%);
     }
-  `;
-  const Content__mobie = styled.div`
-    background: #0a2029;
-    padding-bottom: 2rem;
-    padding-left: 1.5rem;
-    padding-top: 1rem;
-    transform: scale(1.01);
-    position: relative;
-    z-index: 2;
-    span {
-      font-size: 14px;
-      color: #949494;
-    }
-    h1 {
-      font-size: 20px;
-      color: #ffffff;
-    }
-    p {
-      font-size: 14px;
-      color: #949494;
-    }
-    @media (max-width: 736px) {
-      padding-bottom: 1rem;
-    }
-  `;
-  const Content = styled.div`
-    position: absolute;
-    z-index: 10;
-    width: 100%;
+  }
+  @media (max-width: 736px) {
+    display: none;
+  }
+`;
+const DetailTop__mobie = styled.div`
+  overflow: hidden;
+  display: none;
+
+  @media (max-width: 736px) {
     display: block;
-    max-width: 870px;
-    height: 20rem;
+  }
+`;
+const ImgBackground = styled.img`
+  filter: blur(15px);
+  margin: -50% 0 -5px 0px;
+  display: block;
+  width: 100%;
+`;
+const ImgBackgroundMobie = styled.img`
+  margin: -50% 0 -5px 0px;
+  display: block;
+  width: 100%;
+`;
+const Container__ImgBackGroundMobie = styled.div.attrs((props) => ({
+  Height: props.height,
+}))`
+  dislay: none !important;
+  overflow: hidden;
+  margin-top: 4rem;
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+  height: ${(props) => props.Height};
+
+  &::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: linear-gradient(to top, rgb(10, 32, 41), transparent 100%);
+  }
+`;
+const Content__mobie = styled.div`
+  background: #0a2029;
+  padding-bottom: 2rem;
+  padding-left: 1.5rem;
+  padding-top: 1rem;
+  transform: scale(1.01);
+  position: relative;
+  z-index: 2;
+  span {
+    font-size: 14px;
+    color: #949494;
+  }
+  h1 {
+    font-size: 20px;
+    color: #ffffff;
+  }
+  p {
+    font-size: 14px;
+    color: #949494;
+  }
+  @media (max-width: 736px) {
+    padding-bottom: 1rem;
+  }
+`;
+const Content = styled.div`
+  position: absolute;
+  z-index: 10;
+  width: 100%;
+  display: block;
+  max-width: 870px;
+  height: 20rem;
+  top: 50%;
+  left: 50%;
+  transform: translate(-48%, -50%);
+  margin: auto;
+`;
+const Row = styled.div`
+  height: 100%;
+`;
+const ImgCaptionMovie = styled.div`
+  display: block;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 0;
+  left: 0;
+  z-index: 1;
+  img {
+    display: block;
+    color: #ffffff;
+    width: 5rem;
+    height: 5rem;
+    line-height: 5rem;
+    text-align: center;
+    position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-48%, -50%);
-    margin: auto;
-  `;
-  const Row = styled.div`
-    height: 100%;
-  `;
-  const ImgCaptionMovie = styled.div`
-    display: block;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    padding: 0;
-    left: 0;
-    z-index: 1;
-    img {
-      display: block;
-      color: #ffffff;
-      width: 5rem;
-      height: 5rem;
-      line-height: 5rem;
-      text-align: center;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      cursor: pointer;
-    }
-  `;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+  }
+`;
+
+function DetailTop(props) {
   const Iframe = styled.iframe.attrs((props) => ({
     Open: props.isOpen,
   }))`
@@ -145,7 +147,7 @@ function DetailTop(props) {
   };
   return (
     <div>
-      <DetailTop className="position-relative">
+      <DetailTop_Desktop className="position-relative">
         <ImgBackground src={movie.hinhAnh} alt="desktop" />
         <Content>
           <Row className="row">
@@ -153,7 +155,7 @@ function DetailTop(props) {
             <DetailTopRight item={movie}></DetailTopRight>
           </Row>
         </Content>
-      </DetailTop>
+      </DetailTop_Desktop>
       <DetailTop__mobie>
         <Container__ImgBackGroundMobie height={open === "none" ? "20rem" : "0"}>
           <ImgBackgroundMobie src={movie.hinhAnh} alt="mobie" />
@@ -181,6 +183,7 @@ function DetailTop(props) {
           <p>91 phút - 0 IMDb - 2D/Digital</p>
         </Content__mobie>
       </DetailTop__mobie>
+      <ModalComponent />
     </div>
   );
 }
--- src/component/HomeTemplate/DetailPageComponent/DetailTop/DetailTopRight.js
+++ src/component/HomeTemplate/DetailPageComponent/DetailTop/DetailTopRight.js
@@ -1,94 +1,95 @@
 import React from "react";
 import styled from "styled-components";
 import CircleProgressBar from "./CircleProgressBar/CircleProgressBar";
-function DetailTopRight(props) {
-  const Col_9 = styled.div`
-    flex: 0 0 75%;
-    max-width: 75%;
-    margin: 0;
-    padding: 0;
+const Col_9 = styled.div`
+  flex: 0 0 75%;
+  max-width: 75%;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 14px;
+  .left {
+    width: 32rem;
+    color: #e9e9e9;
+    font-weight: 1rem;
+    //padding-right: 21rem;
+    padding-left: 1rem;
+    .pEl-2 {
+      span {
+        font-size: 1rem;
+        display: inline-block;
+        background-color: #fb4226 !important;
+        padding: 0 6px;
+        border-radius: 5px;
+        margin-right: 5px;
+      }
+      font-size: 1.5rem;
+      margin: 0;
+    }
+  }
+  .right {
+    text-align: center;
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
-    font-size: 14px;
-    .left {
-      width: 32rem;
-      color: #e9e9e9;
-      font-weight: 1rem;
-      //padding-right: 21rem;
-      padding-left: 1rem;
-      .pEl-2 {
-        span {
-          font-size: 1rem;
-          display: inline-block;
-          background-color: #fb4226 !important;
-          padding: 0 6px;
-          border-radius: 5px;
-          margin-right: 5px;
-        }
-        font-size: 1.5rem;
-        margin: 0;
-      }
-    }
-    .right {
-      text-align: center;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      transform: translate(6.25rem, 0.625rem);
-      .circular {
-        height: 6.25rem;
-        width: 6.25rem;
-        transform: scale(1.3);
-        // background: red;
-        position: relative;
-        .inner {
-          z-index: 3;
-          display: block;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          height: 87px;
-          width: 87px;
-          border-radius: 100%;
+    transform: translate(6.25rem, 0.625rem);
+    .circular {
+      height: 6.25rem;
+      width: 6.25rem;
+      transform: scale(1.3);
+      // background: red;
+      position: relative;
+      .inner {
+        z-index: 3;
+        display: block;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        height: 87px;
+        width: 87px;
+        border-radius: 100%;
 
-          background-color: rgba(0, 0, 0, 0.4);
-        }
-        .circle {
-        }
-        .numb {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          z-index: 4;
-          font-size: 2rem;
-          font-weight: 1rem;
-          color: #fff;
-        }
+        background-color: rgba(0, 0, 0, 0.4);
       }
-      .img-container {
-        position: relative;
-        display: flex;
-        margin-top: 0;
-        margin-bottom: 5px;
-        img {
-          max-width: 2rem;
-          height: auto;
-          vertical-align: middle;
-          margin: 0 0.1rem;
-        }
+      .circle {
       }
-      .detail {
-        margin-top: 5px;
-        color: #e9e9e9;
-        font-size: 0.9rem;
+      .numb {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 4;
+        font-size: 2rem;
+        font-weight: 1rem;
+        color: #fff;
       }
     }
-  `;
+    .img-container {
+      position: relative;
+      display: flex;
+      margin-top: 0;
+      margin-bottom: 5px;
+      img {
+        max-width: 2rem;
+        height: auto;
+        vertical-align: middle;
+        margin: 0 0.1rem;
+      }
+    }
+    .detail {
+      margin-top: 5px;
+      color: #e9e9e9;
+      font-size: 0.9rem;
+    }
+  }
+`;
+
+function DetailTopRight(props) {
   const { item } = props;
 
   const renderImg = () => {
